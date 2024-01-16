6ebbeba5de93c57d36c7be591a45124b754c176e
Jin0331 <sempre813@naver.com>
2024-01-16 22:16:11 +0900

    [Add] Beer 폴더구조 생성

---
+++ Network Practice/Beer/Model/BeerRandom.swift
@@ -0,0 +1,15 @@
+//
+//  Beer.swift
+//  Network Practice
+//
+//  Created by JinwooLee on 1/16/24.
+//
+
+import UIKit
+import Alamofire
+
+struct BeerRandomAPIManager {
+    func callRequestBeerRandom(){
+        
+    }
+}
