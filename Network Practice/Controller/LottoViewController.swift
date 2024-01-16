a8f4726583a2060b5995869b021c0b28a7ae7c72
Jin0331 <sempre813@naver.com>
2024-01-16 21:03:27 +0900

    [fix] PickerView 및 API 통신 구현

--- Network Practice/Controller/LottoViewController.swift
+++ Network Practice/Controller/LottoViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class LottoViewController: UIViewController {
-
+    
+    @IBOutlet var lottoLabelSet: [UILabel]!
+    @IBOutlet var numberPickerView: UIPickerView!
+    
+    var lottoRequest : Lotto?
+    var allLottoNumber = (1...1024).map{return String($0)}
+    let apiManager = LottoAPIManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        configureProtocol()
+        numberPickerView.selectRow(Int(allLottoNumber[0])! - 1, inComponent: 0, animated: true)
+        
     }
-
-
+    
 }
 
+
+extension LottoViewController : UIPickerViewDelegate, UIPickerViewDataSource {
+    func configureProtocol() {
+        numberPickerView.delegate = self
+        numberPickerView.dataSource = self
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(allLottoNumber[row])회"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return allLottoNumber.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(#function)
+        
+        // 특정회차 값 가져오기
+        apiManager.callRequestNumber(number: allLottoNumber[row]) { value in
+            self.lottoRequest = value
+            print(self.lottoRequest)
+            guard let t = self.lottoRequest else {return}
+            self.lottoLabelSet[0].text = String(t.drwtNo1)
+        }
+    }
+}
